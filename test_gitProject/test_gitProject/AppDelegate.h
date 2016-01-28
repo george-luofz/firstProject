85f9d8d0e232e2ce023ac44734f0027d3691672f
george-luofz <2372116403@qq.com>
2016-01-28 12:20:50 +0800

    first commit

---
+++ test_gitProject/test_gitProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_gitProject
+//
+//  Created by Luo on 16/1/28.
+//  Copyright © 2016年 Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
